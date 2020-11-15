2d6f817a8480082c045c3b6aa8b0f4db2983e78d
Moisés <moalgueji@hotmail.com>
2020-11-15 14:52:21 -0500

    Actualicé el archivo con los cambios de Humberto y las tarjetas blancas

--- frontend_V2.py
+++ frontend_V2.py
@@ -10,6 +10,11 @@ import pandas as pd
 import random
 from dash.dependencies import Input, Output
 
+
+
+
+
+
 #################### START OF "app" ########################
 app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.config.suppress_callback_exceptions = True
@@ -49,6 +54,9 @@ mini_container = {
 # Tabla principal Completa
 df_orig = pd.read_csv('datos_con_coordenadas_v1 (1).csv', compression='gzip')
 df = df_orig.copy() # Copia de la tabla original
+clusters_df = pd.read_csv("cluster.csv")
+df = df.merge(clusters_df[['cluster', 'Tienda']], on='Tienda')
+df['cluster']= df['cluster'].astype('str')
 
 # 
 df_tiendas_mapa = pd.read_csv("Tiendas_mapa.csv")
@@ -107,18 +115,226 @@ def crear_carousel(product_list):
         }])
     return carrusel
 
+
+
+#--------------------------------------------------------------------------------------------------------------------------
+#											Calculo de estadisticas
+#--------------------------------------------------------------------------------------------------------------------------
+
+
+num_tiendas = str(len(df['Tienda'].unique()))
+num_cats = str(len(df['NOMBRE CAT'].unique()))
+num_subs = str(len(df['NOMBRE SUB'].unique())) 
+num_fabricantes = str(len(df['Nombre Fabricante'].unique()))
+num_productos = str(len(df['Material'].unique()))
+num_registros = str(len(df))
+
+
+br = html.Br()
+
+
+#--------------------------------------------------------------------------------------------------------------------------
+#											Calculo de datos para evolucion
+#--------------------------------------------------------------------------------------------------------------------------
+
+def set_mes_number(x):
+
+    mes_dic = {'Enero 2019':1, 'Abril 2019':4, 'Mayo 2019':5, 'Noviembre 2019':11,
+            'Diciembre 2019':12, 'Junio 2020':18, 'Agosto 2020':20, 'Enero 2020':13,
+            'Febrero 2020':14, 'Febrero 2019':2, 'Julio 2019':7, 'Septiembre 2019':9,
+            'Marzo 2020':15, 'Marzo 2019':3, 'Mayo 2020':17, 'Abril 2020':16,
+            'Octubre 2019':10, 'Julio 2020':19, 'Junio 2019':6, 'Agosto 2019':8}
+
+    return mes_dic[x]
+
+
+df['mes_number'] = df['MES'].apply(set_mes_number)
+
+
+#--------------------------------------------------------------------------------------------------------------------------
+#											Generacion de tarjetas
+#--------------------------------------------------------------------------------------------------------------------------
+
+
+card_tiendas=   dbc.Card([
+            dbc.CardHeader("Numero de Tiendas"),
+            dbc.CardBody([
+                    html.H4(num_tiendas + " Tiendas", className="card-title"),
+                    
+            ])],
+        style={"width": "14rem"}, color='info', outline=True)
+
+card_cats=   dbc.Card([
+            dbc.CardHeader("Numero de Categorias"),
+            dbc.CardBody([
+                    html.H4(num_cats + " Categorias", className="card-title"),
+                    
+            ])],
+        style={"width": "14rem"}, color='info', outline=True)
+
+card_subs=   dbc.Card([
+            dbc.CardHeader("Numero de Subcategorias"),
+            dbc.CardBody([
+                    html.H4(num_subs + " Subcategorias", className="card-title"),
+                    
+            ])],
+        style={"width": "14rem"}, color='info', outline=True)
+
+
+card_productos= dbc.Card([
+            dbc.CardHeader("Numero de Productos"),
+            dbc.CardBody([
+                    html.H4(num_productos + " Productos", className="card-title"),
+                    
+            ])],
+        style={"width": "14rem"}, color='info', outline=True)
+
+card_fabricantes = dbc.Card([
+            dbc.CardHeader("Numero de Fabricantes"),
+            dbc.CardBody([
+                    html.H4(num_fabricantes + " Fabricantes", className="card-title"),
+                    
+            ])],
+        style={"width": "14rem"}, color='info', outline=True)
+
+
+card_registros = dbc.Card([
+            dbc.CardHeader("Numero de Registros"),
+            dbc.CardBody([
+                    html.H4(num_registros + " Registros", className="card-title"),
+                    
+            ])],
+        style={"width": "14rem"}, color='info', outline=True)
+
+
+#--------------------------------------------------------------------------------------------------------------------------
+#											Generacion de panel completo estadisticas
+#--------------------------------------------------------------------------------------------------------------------------
+
+estadisticas =  html.Div(children=[
+                br, html.H4('ESTADISTICAS GENERALES DEL CONJUNTO DE DATOS'), br,
+                dbc.Row(
+                    [
+                        dbc.Col(card_tiendas),
+                        dbc.Col(card_cats),
+                        dbc.Col(card_subs),
+                    
+                    ], className="mb-2", align='center', justify='center'),
+
+                dbc.Row(
+                    [
+                        dbc.Col(card_productos),
+                        dbc.Col(card_fabricantes),
+                        dbc.Col(card_registros),
+                    
+                    ], className="mb-2", align='center', justify='center'),
+                ]) # Cierro el div 
+
+
+
+#--------------------------------------------------------------------------------------------------------------------------
+#											Generacion de tarjetas
+#--------------------------------------------------------------------------------------------------------------------------
+
+
+card_moises=   dbc.Card([
+            dbc.CardHeader("Moises Guerrero"),
+            dbc.CardBody([
+                    html.H4("Ing. Electronico", className="card-title"),
+                    
+            ])],
+        style={"width": "14rem"}, color='info', outline=True)
+
+card_katerine=   dbc.Card([
+            dbc.CardHeader("Katherine ________"),
+            dbc.CardBody([
+                    html.H4("Ing. Sonido", className="card-title"),
+                    
+            ])],
+        style={"width": "14rem"}, color='info', outline=True)
+
+card_rafael=   dbc.Card([
+            dbc.CardHeader("Rafael Rojas"),
+            dbc.CardBody([
+                    html.H4("Ing. Industrial", className="card-title"),
+                    
+            ])],
+        style={"width": "14rem"}, color='info', outline=True)
+
+
+card_juan= dbc.Card([
+            dbc.CardHeader("Juan Esteban"),
+            dbc.CardBody([
+                    html.H4("Ingeniero ______", className="card-title"),
+                    
+            ])],
+        style={"width": "14rem"}, color='info', outline=True)
+
+card_mariaalejandra = dbc.Card([
+            dbc.CardHeader("Maria Alejandra"),
+            dbc.CardBody([
+                    html.H4("Ing. Sistemas", className="card-title"),
+                    
+            ])],
+        style={"width": "14rem"}, color='info', outline=True)
+
+
+card_heidi = dbc.Card([
+            dbc.CardHeader("Heidi _______"),
+            dbc.CardBody([
+                    html.H4("Ing. Industrial", className="card-title"),
+                    
+            ])],
+        style={"width": "14rem"}, color='info', outline=True)
+
+card_humberto = dbc.Card([
+            dbc.CardHeader("Humberto Ramos"),
+            dbc.CardBody([
+                    html.H4(num_registros + "Ing. Electronico", className="card-title"),
+                    
+            ])],
+        style={"width": "14rem"}, color='info', outline=True)
+
+
+#--------------------------------------------------------------------------------------------------------------------------
+#											Generacion de panel completo de creditos
+#--------------------------------------------------------------------------------------------------------------------------
+
+creditos =  html.Div(children=[
+                br, html.H4('PROYECTO REALIZADO POR:'), br,
+                dbc.Row(
+                    [
+                        dbc.Col(card_moises),
+                        dbc.Col(card_katerine),
+                        dbc.Col(card_rafael),
+                    
+                    ], className="mb-2", align='center', justify='center'),
+
+                dbc.Row(
+                    [
+                        dbc.Col(card_humberto),
+                        dbc.Col(card_heidi),
+                        dbc.Col(card_mariaalejandra),
+                        dbc.Col(card_juan),
+                    
+                    ], className="mb-2", align='center', justify='center'),
+                ]) # Cierro el div 
+
+
+
+
 ######################### SIDEBAR LAYOUT ############################
 sidebar = html.Div(
     [
     #html.H2("Sidebar", className="Sidebar-Logo"),
     html.Img(className="logo_mintic", src="https://colombiatic.mintic.gov.co/679/channels-581_logo_footer_mintic.png",
-        style={'height':'12%','align':'center'}),
+        style={'height':'8%','align':'center'}),
     html.Hr(),
     dbc.Nav(
         [
         dbc.NavLink("Recomendación", href="/page-1", id="page-1-link"),
         dbc.NavLink("Estadísticas", href="/page-2", id="page-2-link"),
-        dbc.NavLink("Page 3", href="/page-3", id="page-3-link"),
+        dbc.NavLink("Creditos", href="/page-3", id="page-3-link"),
         ],
         vertical=True,
         pills=True,
@@ -163,11 +379,7 @@ homepage_layout = html.Div(
             html.Img(className="Imagen_slider5",
                    src="https://static.wixstatic.com/media/e9606e_7f2dde5a4cc541859bdf5599d4b2562d~mv2.png"),
             html.Img(className="Imagen_slider6",
-                   src="https://static.wixstatic.com/media/e9606e_adf6f666ace4425f9aca6cdf6dd0c732~mv2.png"),
-            html.Img(className="Imagen_slider7",
-                   src="https://static.wixstatic.com/media/e9606e_a01463d424f8451b93e125b093aa2a71~mv2.png"),
-            html.Img(className="Imagen_slider8",
-                   src="https://static.wixstatic.com/media/e9606e_a01463d424f8451b93e125b093aa2a71~mv2.png"),            
+                   src="https://static.wixstatic.com/media/e9606e_adf6f666ace4425f9aca6cdf6dd0c732~mv2.png"),           
         ],
             slides_to_scroll=1,
             swipe_to_slide=True,
@@ -209,8 +421,59 @@ homepage_layout = html.Div(
                 html.Div(id='area_recomendaciones', children=[])
                 ],style=mini_container),
             ]),
-        ]), # AQUI TERMINA LA SEGUNDA FILA DONDE ESTÁ EL MAPA
-    ])
+       
+
+#--------------------------------------------------------------------------------------------------------------------------
+#											Generacion de graficas de evolución de compras
+#--------------------------------------------------------------------------------------------------------------------------
+
+        dbc.Row([
+       
+           dbc.Col([
+               
+               #Titulo en Fila 2 - Col 1
+               html.H2(className="h2-title", children="Gráfica No. 1"),
+               html.H4(className="h4-title", children="Evolución de Compras Tienda", style={'font-weight':'bold'}),
+               dcc.Graph(id='grafico_1', figure={}),
+                
+               
+           ], md=6, width="auto", align='center'), # Fin de fila 2 Col 1
+           
+           
+           dbc.Col([
+            
+               #Titulo en Fila 2 - Col 2
+
+               html.H2(className="h2-title", children="Gráfica No. 2"),
+               html.H4(className="h4-title", children="Evolución de Compras Cluster", style={'font-weight':'bold'}),
+               dcc.Graph(id='grafico_2', figure={}),
+                       
+           ], md=6, width="auto"), # Fin de fila 2 Col 2
+           
+           html.Div(className='dropdown-menu show', children=[
+                   
+                   dbc.RadioItems(
+                       className="example-radios-row",
+                       id='menu_grafico_1',
+                       options=[
+                            {'label': 'Materiales ', 'value': 'Nombre Material'},
+                            {'label': 'Subcategorias ', 'value': 'NOMBRE SUB'},
+                            {'label': 'Categorias ', 'value': 'NOMBRE CAT'}
+                        ],
+                        value='NOMBRE SUB',      
+                    ),
+                      
+               ], style=mini_container),
+           
+       ], align='center', justify='center'), # fin de generación de tablas de evolución
+        
+        
+    
+    ], fluid=True), # AQUI TERMINA EL CONTAINER PPAL
+        
+
+        
+    ]) # AQUI TERMINA EL DIV PPAL DEL LAYOUT
 ######################### CALLBACKS ###########################
 
 ################## CALLBACKS SIDEBAR #####################
@@ -233,9 +496,9 @@ def render_page_content(pathname):
     if pathname in ["/", "/page-1"]:
         return homepage_layout
     elif pathname == "/page-2":
-        return html.P("This is the content of page 2. Yay!")
+        return  html.Center(children=[br, estadisticas, br,]),
     elif pathname == "/page-3":
-        return html.P("Oh cool, this is page 3!")
+        return html.Center(children=[br, creditos, br,]),
     # If the user tries to reach a different page, return a 404 message
     return dbc.Jumbotron(
         [
@@ -345,5 +608,73 @@ def grafico_mapa(input_tienda):
            
     return fig_map
 
+
+# ----------------------------------------------------------------------------
+#                             Callbacks e interactividad
+#-----------------------------------------------------------------------------
+
+
+
+@app.callback(
+    
+        Output(component_id="grafico_1",component_property="figure"),
+
+    
+    [
+        Input(component_id="dropdown_tienda", component_property="value"),
+        Input(component_id="menu_grafico_1", component_property="value"),
+    ],
+)
+def grafico_categorias_tienda(input_tienda,filtro):
+    
+    Nombre_Tienda = df[df['Tienda']==int(input_tienda)]['Nombre Tienda'].max()
+    
+    dfsbar = df[df['Tienda']==int(input_tienda)]
+    dfsbar =pd.crosstab(dfsbar['mes_number'], dfsbar[filtro]).reset_index()
+    fig_cat_tienda = px.bar(dfsbar, x="mes_number", y=dfsbar.columns, title=("Evolución compras "+ Nombre_Tienda))
+    
+    
+    fig_cat_tienda.update_layout(
+        title=("Evolución compras "+ Nombre_Tienda),
+    )
+    
+    #fig_cat_tienda.show()
+    
+    
+    return fig_cat_tienda
+
+
+
+@app.callback(
+    
+        Output(component_id="grafico_2",component_property="figure"),
+    
+    [
+        Input(component_id="dropdown_tienda", component_property="value"),
+        Input(component_id="menu_grafico_1", component_property="value"),
+    ],
+)
+def grafico_categorias_tienda_cluster(input_tienda,filtro):
+    
+    Cluster = df[df['Tienda']==int(input_tienda)]['cluster'].max()
+    
+    dfsbar2 = df[df['cluster']==Cluster]
+    dfsbar2 =pd.crosstab(dfsbar2['mes_number'], dfsbar2[filtro]).reset_index()
+    fig_cat_tienda_cluster = px.bar(dfsbar2, x="mes_number", y=dfsbar2.columns, title=("Evolución compras Cluster "+ Cluster))
+    fig_cat_tienda_cluster.update_layout(
+        title=("Evolución compras Cluster "+ Cluster),
+        
+    )
+    
+    return fig_cat_tienda_cluster
+
+
+
+
+
+
+
 if __name__ == "__main__":
     app.run_server(host='0.0.0.0', port='8050', debug=True)
+    
+    
